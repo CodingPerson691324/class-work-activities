1d1f0568ced3bb228c7bd41b6cca315ab08cfea1
CodingPerson691324 <praveen.tapashetti@gmail.com>
2025-03-17 16:53:48 +0000

    initial commit

---
+++ L2-A1.py
@@ -0,0 +1,10 @@
+#creating different variables
+x = 5
+y = 'john'
+print(x)
+print(y)
+codingal = 'codingal'
+print(codingal)
+#user input
+name = input('enter your name ')
+print('\n hello',name,'\n welcome to codingal')
