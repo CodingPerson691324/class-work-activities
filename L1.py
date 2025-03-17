49fa76135ddfe48f17ce0cfae3e73509980c3d30
CodingPerson691324 <praveen.tapashetti@gmail.com>
2025-03-17 16:52:02 +0000

    initial commit

---
+++ L1.py
@@ -0,0 +1,10 @@
+#print a sentence
+print("welcome to the world of programming")
+#print a number
+print(4)
+#next line after print
+print("hello python \n")
+#pass multiple arguments for printing
+print("hello",3)
+#end argument of print statment
+print("welcome to",end = "*")
