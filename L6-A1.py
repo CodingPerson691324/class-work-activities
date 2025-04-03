6a0f98443a25bd13531cfe81ac4311057118d297
CodingPerson691324 <praveen.tapashetti@gmail.com>
2025-04-03 18:07:22 +0100

    initial commit

---
+++ L6-A1.py
@@ -0,0 +1,19 @@
+a = 0
+b = 5
+c = 6
+if a and b and c:
+    print('all the nhumbers have boolean values as true')
+else:
+    print('at least one of the numbers have a boolean value of false')
+
+a = 10
+b = -10
+c = 0
+if a>0 or b>0:
+    print('either of the numbers are greater that 0')
+else:
+    print('no number is greater than 0')
+if b>0 or c>0:
+    print('either of the numbers are greater that 0')
+else:
+    print('no number is greater than 0')
